f3d9237a10548b8d368c2ab0ef2385009cafadab
Philippe Normand <philn@igalia.com>
2023-05-21 16:10:56 +0100

    scripts: Add wk-shell
    Useful especially in local-deps runtime environment

--- scripts/install-launchers.sh
+++ scripts/install-launchers.sh
@@ -2,6 +2,7 @@
 
 mv /scripts/webkit-clangd /usr/local/bin/
 mv /scripts/wk-launcher /usr/local/bin/
+mv /scripts/wk-shell /usr/local/bin/
 ln -s /usr/local/bin/wk-launcher /usr/local/bin/run-minibrowser
 ln -s /usr/local/bin/wk-launcher /usr/local/bin/run-webkit-tests
 ln -s /usr/local/bin/wk-launcher /usr/local/bin/test-webkitpy
