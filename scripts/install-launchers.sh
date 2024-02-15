e7c8bf721e6f92208a82cd8b4fd0279f5d5a2a5c
Philippe Normand <philn@igalia.com>
2024-02-15 18:22:19 +0000

    Add webkit-clangd-indexer

--- scripts/install-launchers.sh
+++ scripts/install-launchers.sh
@@ -5,6 +5,7 @@ DEST_BIN=/usr/local/bin
 mv /scripts/webkit-clangd $DEST_BIN
 mv /scripts/wk-launcher $DEST_BIN
 mv /scripts/wk-shell $DEST_BIN
+mv /scripts/webkit-clangd-indexer $DEST_BIN
 ln -s /usr/local/bin/wk-launcher $DEST_BIN/run-minibrowser
 ln -s /usr/local/bin/wk-launcher $DEST_BIN/run-webkit-tests
 ln -s /usr/local/bin/wk-launcher $DEST_BIN/test-webkitpy
