7736f20f0b2bda69eb1f111137d308fc826bb660
Philippe Normand <philn@igalia.com>
2024-08-15 11:58:45 +0100

    Add rewrite-wk-gst-log script

--- scripts/install-launchers.sh
+++ scripts/install-launchers.sh
@@ -6,6 +6,7 @@ mv /scripts/webkit-clangd $DEST_BIN
 mv /scripts/wk-launcher $DEST_BIN
 mv /scripts/wk-shell $DEST_BIN
 mv /scripts/webkit-clangd-indexer $DEST_BIN
+mv /scripts/rewrite-wk-gst-log $DEST_BIN
 ln -s /usr/local/bin/wk-launcher $DEST_BIN/run-minibrowser
 ln -s /usr/local/bin/wk-launcher $DEST_BIN/run-webkit-tests
 ln -s /usr/local/bin/wk-launcher $DEST_BIN/test-webkitpy
